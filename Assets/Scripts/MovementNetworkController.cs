9eb2732e36f13edeeabc5a56c7ed8b9dba8ac9df
habvielzutun <gerlachmelvin@web.de>
2025-01-26 20:34:40 +0100

    Uptime Error etc

--- Assets/Scripts/MovementNetworkController.cs
+++ Assets/Scripts/MovementNetworkController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MovementNetworkController : NetworkBehaviour
 {
     public NetworkVariable<Vector3> Position = new();
+    public float jumpForce = 5f;
 
 
     [Rpc(SendTo.Server)]
@@ -21,6 +22,22 @@ public class MovementNetworkController : NetworkBehaviour
 
             Vector3 movement = new Vector3(moveX, 0f, moveZ) * 5 * Time.deltaTime;
             transform.Translate(movement, Space.World);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+
+            float rotationSpeed = 100f;
+            if (Input.GetKey(KeyCode.Q))
+            {
+                transform.Rotate(0f, -rotationSpeed * Time.deltaTime, 0f);
+            }
+            if (Input.GetKey(KeyCode.E))
+            {
+                transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+            }
+
             SubmitPositionRequestServerRpc(transform.position);
         }
 
